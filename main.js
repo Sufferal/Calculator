a969918e686d616441f890715ef131782c3ef642
Ciprian <botnari_ciprian@mail.ru>
2022-04-27 09:24:04 +0300

    Working operators
    All operators work, but still need further improvement.
    Things to fix:
    1. input can receive letters
    2. can't add two decimal numbers
    3. floating point error addition 0.1+0.2=0.3000000000004
    4. you can type multiple zeros at the beginning
    5. first character can be an operator

--- main.js
+++ main.js
@@ -4,21 +4,60 @@ let result = document.getElementById('result');
 // Buttons
 let buttons = Array.from(document.getElementsByClassName('btn'));
 
+// Operators
+let operators = ['+', '-', '*', '/', '.'];
+
+function isOperator(val) {
+  return (operators.includes(val)) ? true : false;
+}
+
 buttons.forEach(btn => {
   btn.addEventListener('click', e => {
-    currentChar = e.target.value;
+    currentValue = e.target.value;
 
-    switch (currentChar) {
-      case 'C':
+    switch (currentValue) {
+      case 'clear':
         result.value = '';
         break;
 
-      // case 'C':
-      //   result.value = ;
-      //   break;
+      case 'delete':
+        result.value = result.value.slice(0, -1);
+        break;
+
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+        if(!isOperator(result.value.slice(-1))) {
+          result.value += currentValue;
+          break;
+        }
+        break;
+
+      case '.':
+        if(!result.value.includes('.')) {
+          result.value += currentValue;
+          break;
+        }
+        break;
+      
+      case 'root':
+        if(!isOperator(result.value.slice(-1))) {
+          result.value = Math.sqrt(result.value);
+          break;
+        }
+        break;
+        
+
+      case '=':
+        if(!isOperator(result.value.slice(-1))) {
+          result.value = eval(result.value);
+          break;
+        }
+        break;
     
       default:
-        result.value += currentChar;
+        result.value += currentValue;
         break;
     }
   });
